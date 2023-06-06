b8c3a311140a63663e0763b890f424e899ba1108
miftahullislam <miftahullislamsohan@gmail.com>
2023-06-06 09:38:23 +0600

    Services section added

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -2,14 +2,56 @@
 
 const Services = () => {
     return (
-        <div>
-            <div>
-                <div className="text-center my-5">
-                    <h1 className="font-bold text-4xl text-center underline mb-4">Our <span className="text-[#FFB6C6] "> Services</span></h1>
-                    <p>The doll toy is adorable and well-made, providing hours of imaginative play for my child. This doll toy exceeded <br /> my expectations with its realistic. loves this doll toy and carries it everywhere.</p>
-                </div>
+        <div  className="flex flex-col items-center justify-center">
+
+            <div className="text-center mt-6">
+                <h1 className="font-bold text-4xl text-center underline mb-4">Our <span className="text-[#FFB6C6] "> Services</span></h1>
+                <p>Feel free to customize it according to your preferences and the nature of your doll toy services. <br /> my expectations with its realistic. loves this doll toy and carries it everywhere.</p>
             </div>
+
+
+            <div className="md:h-48 grid md:grid-cols-4 justify-center gap-14 items-center text-center">
+
+                <div className="flex gap-3">
+                    <div><img className="w-20 h-16" src="https://img.freepik.com/premium-vector/bubble-chat-talk-with-wrench-logo-design-vector-graphic-symbol-icon-illustration-creative-idea_15473-12903.jpg" alt="" /></div>
+                    <div>
+                        <h2 className="uppercase text-xl text-left">Shipping Services</h2>
+                        <h5 className="text-left">Fast Shipping</h5>
+                    </div>
+                </div>
+
+
+                <div className="flex gap-3">
+                    <div><img className="w-20 h-16" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTda8R5wsDexOmNtviqhLPDJLgk0k_cmTh7cg&usqp=CAU" alt="" /></div>
+                    <div>
+                        <h2 className="uppercase text-xl text-left">Online Payment</h2>
+                        <h5 className="text-left">Payment Methods</h5>
+                    </div>
+                </div>
+                <div className="flex gap-3">
+                    <div><img className="w-20 h-16" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuDqLxlumPSIxvcbtRnMnT0roXK5bxtQAX8A&usqp=CAU" alt="" /></div>
+                    <div>
+                        <h2 className="uppercase text-xl text-left">24 Hours Support</h2>
+                        <h5 className="text-left">Unlimited Help </h5>
+                    </div>
+                </div>
+                <div className="flex gap-3">
+                    <div><img className="w-20 h-16" src="https://img.freepik.com/premium-vector/express-delivery-label-courier-boy-riding-motor-bike-vector-logo_212474-687.jpg" alt="" /></div>
+                    <div>
+                        <h2 className="uppercase text-xl text-left">On time delivery</h2>
+                        <h5 className="text-left">All Time Delivery</h5>
+                        
+                    </div>
+                </div>
+
+            </div>
+
         </div>
+
+
+
+
+
     );
 };
 
